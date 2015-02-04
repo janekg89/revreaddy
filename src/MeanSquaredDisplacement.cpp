2ec7b016148024c028dd05721715316ce4cb9243
chrisfroe <christoph.froehner@fu-berlin.de>
2015-02-04 18:33:07 +0100

    Added efficiency scenario. Future implementation will have an extra module utils which has some useful functions for saving/loading particle configurations and displaying information on the current system.

---
+++ src/MeanSquaredDisplacement.cpp
@@ -0,0 +1,93 @@
+/* MeanSquaredDisplacement.cpp */
+
+#include "MeanSquaredDisplacement.h"
+
+MeanSquaredDisplacement::MeanSquaredDisplacement(unsigned int id)
+{
+	this->recPeriod = 1;
+	this->clearPeriod = 0;
+	this->particleTypeId = id;
+}
+
+MeanSquaredDisplacement::~MeanSquaredDisplacement()
+{
+	this->recPeriod = 0;
+}
+
+void MeanSquaredDisplacement::record(
+	std::vector<Particle>& activeParticles,
+	unsigned long int t)
+{
+	if (this->meanSquaredDisplacements.size() == 0) {
+		for (int i=0; i<activeParticles.size(); i++) {
+			if (activeParticles[i].typeId == this->particleTypeId) {
+				positionTuple pt;
+				pt.position = activeParticles[i].position;
+				pt.boxCoordinates = activeParticles[i].boxCoordinates;
+				this->startPoints.push_back(pt);
+			}
+		}
+	}
+	std::vector<double> displacement = {0.,0.,0.};
+	std::vector<long>   relativeBoxCoordinates = {0,0,0};
+	std::vector<double> squaredDisplacements;
+	double squaredDisplacement = 0.;
+	double mean = 0.;
+	unsigned int index = 0;
+	for (int i=0; i<activeParticles.size(); i++) {
+		if (activeParticles[i].typeId == this->particleTypeId) {
+			relativeBoxCoordinates[0] = activeParticles[i].boxCoordinates[0] 
+			                          - startPoints[index].boxCoordinates[0];
+			relativeBoxCoordinates[1] = activeParticles[i].boxCoordinates[1]
+			                          - startPoints[index].boxCoordinates[1];
+			relativeBoxCoordinates[2] = activeParticles[i].boxCoordinates[2]
+			                          - startPoints[index].boxCoordinates[2];
+			displacement[0] = (double) relativeBoxCoordinates[0] 
+			                * this->boxsize
+			                + activeParticles[i].position[0]
+			                - startPoints[index].position[0];
+			displacement[1] = (double) relativeBoxCoordinates[1]
+			                * this->boxsize
+			                + activeParticles[i].position[1]
+			                - startPoints[index].position[1];
+			displacement[2] = (double) relativeBoxCoordinates[2]
+			                * this->boxsize
+			                + activeParticles[i].position[2]
+			                - startPoints[index].position[2];
+			squaredDisplacement = displacement[0]*displacement[0]
+			                    + displacement[1]*displacement[1]
+			                    + displacement[2]*displacement[2];
+			squaredDisplacements.push_back(squaredDisplacement);
+			mean += squaredDisplacement;
+			index += 1;
+		}
+	}
+	mean /= (double) this->startPoints.size();
+	this->meanSquaredDisplacements.push_back(mean);
+	double standardDeviation = 0.;
+	for (int j=0; j<squaredDisplacements.size(); j++) {
+		standardDeviation += (squaredDisplacements[j] - mean)
+		                   * (squaredDisplacements[j] - mean);
+	}
+	standardDeviation /= (double) (this->startPoints.size() - 1);
+	double standardError = standardDeviation 
+	                     / (double) this->startPoints.size();
+	standardDeviation = sqrt(standardDeviation);
+	standardError     = sqrt(standardError);
+	this->standardDeviations.push_back(standardDeviation);
+	this->standardErrors.push_back(standardError);
+}
+
+void MeanSquaredDisplacement::writeBufferToFile()
+{
+	std::ofstream file;
+	file.open(this->filename);
+	file << "Number of particles: " << startPoints.size() << "\n";
+	file << "Mean Squared Displacement\tStandardDeviation\tStandardError\n";
+	for (int i=0; i<meanSquaredDisplacements.size(); i++) {
+		file << meanSquaredDisplacements[i] << "\t";
+		file << standardDeviations[i] << "\t";
+		file << standardErrors[i] << "\n";
+	}
+	file.close();
+}
