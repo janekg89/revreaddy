7a6b73aae36df66c3986e16aa4d54ca56842cfb8
chrisfroe <christoph.froehner@fu-berlin.de>
2015-03-26 14:28:58 +0100

    Added documentation.

--- utils.py
+++ utils.py
@@ -1,15 +1,65 @@
+"""
+The utils module provides necessary tools when working with revreaddy. 
+Saving and loading the state of a simulation is one of the tasks of
+this module. At a given time during your simulation the state of the 
+system can be partitioned into four parts:
+	* type dictionary, telling which particle type has which properties
+	* positions, a structure holding a typeId and xyz coordinates for
+	  every particle
+	* reactions, a list of possible reactions
+	* general properties of the system
+
+.. note::
+	Observables are not part of the underlying system itself 
+	and will not be saved.
+
+These four are saved into a single hdf5_ file. In this module 
+hdf5 files are generated and read using h5py_. A typical saved file 
+is organised as follows::
+
+	file/
+	-- typedict/
+	   -- numberOfTypes, (1), uint64
+	   -- names, (numberOfTypes), string
+	   -- radii, (numberOfTypes), float64
+	   -- diffusionConstants, (numberOfTypes), float64
+	   -- reactionRadii, (numberOfTypes), float64
+	-- xyz/
+	   -- numberOfParticles, (1), uint64
+	   -- typeIds, (numberOfParticles), uint64
+	   -- positions, (numberOfParticles, uint64
+	-- properties/
+	   -- temperature, (1), float64
+	   -- boxsize, (1), float64
+	   -- isReversible, (1), bool
+	   -- isPeriodic, (1), bool
+	   -- timestep, (1), float64
+	   -- repulsionStrength, (1), float64
+	-- reactions/ 
+		... (to be continued)
+
+``foo/``  marks a group and ``bar, (shape), type`` marks a 
+dataset named bar with a given shape and datatype. For more 
+information on hdf5 organisation/manipulation, see [h5py_].
+
+.. _hdf5: http://www.hdfgroup.org/HDF5/whatishdf5.html
+.. _h5py: http://www.h5py.org
+
+"""
 import numpy as np
 import sim
 import h5py
-# The state of a simulation consists of four parts:
-#   * type dictionary, telling which particle type has which properties
-#   * positions, xyz format
-#   * reactions, a list of possible reactions
-#	* general properties; temperature, boxsize, reversible,
-#	  periodic, timestep
-# These are put into hdf5 files!
-
 def saveSimulation(filename, simulation):
+	"""
+	Save the state of a simulation to file given
+	its *simulation* object.
+
+	:param filename: the file will be given this name
+	:type filename: string
+	:param simulation: object representing your simulation
+	:type simulation: pySimulation
+
+	"""
 	fileHandle = h5py.File(filename, "w")
 
 	# retrieve typedict info from simulation
@@ -24,8 +74,14 @@ def saveSimulation(filename, simulation):
 	typedict["numberOfTypes"][0] = numberOfTypes
 	typedict.create_dataset("names", (numberOfTypes,), dtype="S30")
 	typedict.create_dataset("radii", (numberOfTypes,), dtype=np.float64)
-	typedict.create_dataset("diffusionConstants", (numberOfTypes,), dtype=np.float64)
-	typedict.create_dataset("reactionRadii", (numberOfTypes,), dtype=np.float64)
+	typedict.create_dataset(
+		"diffusionConstants",
+		(numberOfTypes,),
+		dtype=np.float64)
+	typedict.create_dataset(
+		"reactionRadii",
+		(numberOfTypes,),
+		dtype=np.float64)
 	for i in range(numberOfTypes):
 		typedict["names"][i] = names[i]
 		typedict["radii"][i] = radii[i]
