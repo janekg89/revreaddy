3a69c690e2570abbaea4b6da82ac5b92fc73edcc
chrisfroe <christoph.froehner@fu-berlin.de>
2015-04-14 13:53:19 +0200

    Added Energy.cpp

---
+++ src/Energy.cpp
@@ -0,0 +1,9 @@
+/* Energy.cpp */
+
+#include "Energy.h"
+
+Energy::Energy(unsigned int inRecPeriod, unsigned int inClearPeriod)
+{
+	this->recPeriod = inRecPeriod;
+	this->clearPeriod = inClearPeriod;
+}
