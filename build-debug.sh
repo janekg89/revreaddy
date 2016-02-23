b8fdb091ab4677fa70a9c8ae95cb4f5db26b8d00
chrisfroe <christoph.froehner@fu-berlin.de>
2016-02-23 14:21:08 +0100

    some new ignores. added googletest to cmake files and build scripts.

--- build-debug.sh
+++ build-debug.sh
@@ -1,5 +1,9 @@
 mkdir -p build/Debug
 cd build/Debug
-cmake ../.. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DREVREADDY_LOGGING_SEVERITY:STRING=__DEBUG__
+cmake ../.. \
+	-G "Unix Makefiles" \
+	-DCMAKE_BUILD_TYPE=Debug \
+	-DREVREADDY_LOGGING_SEVERITY:STRING=__DEBUG__ \
+	-DREVREADDY_RUN_TESTS:BOOL=ON
 make -j
 cd ../..
--- build-release.sh
+++ build-release.sh
@@ -1,5 +1,9 @@
 mkdir -p  build/Release
 cd build/Release
-cmake ../.. -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DREVREADDY_LOGGING_SEVERITY:STRING=__ERROR__
+cmake ../.. \
+	-G "Unix Makefiles" \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DREVREADDY_LOGGING_SEVERITY:STRING=__ERROR__ \
+	-DREVREADDY_RUN_TESTS:BOOL=OFF
 make -j
 cd ../..
